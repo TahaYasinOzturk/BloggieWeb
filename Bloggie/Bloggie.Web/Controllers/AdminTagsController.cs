ee331e5c404344f84988ea545aa3887cd6c40ea6
TahaYasinOzturk <tahayasinozturk@hotmail.com>
2023-04-23 00:47:07 +0300

    Add view has been created
    The post method on this view is defined in the controller. The data in the view has been transferred to the controls.

--- Bloggie/Bloggie.Web/Controllers/AdminTagsController.cs
+++ Bloggie/Bloggie.Web/Controllers/AdminTagsController.cs
@@ -5,11 +5,29 @@ namespace Bloggie.Web.Controllers
     public class AdminTagsController : Controller
     {
         //yapmıs oldugumuz MVC routing sekli sunun gibi olacaktır.  Admintags controller
-        //Add de action tarafı
+        //Add de action tarafı [HttpGet] arka planda calısır.
         //http://localhost:154/AdminTags/Add
         public IActionResult Add()
         {
             return View();
+        }
+        //not [] alttakinin özelligi belirtiyoruz post olduugnu view da iise submite e basıldıktan sonra view add diyoruz o sayfada tutmasını istiyoruz.
+        //Http Post metodu çalıştırıyoruz. Burada amacımız Add View ında oluşturmus oldugumuz form içerisinde gerçekleşen POST metodu butona tıklandıgında submit olsun. Ve bu submit sonrasında asagidaki action ımız devreye girsin.
+        //Peki kullanıcının bu bahsi gecen view üzerinde inputlara girmiş oldugu verileri sayfamıza nasıl cekecegiz?
+        //1.Yöntem Bunun icin önce view a gidecegiz ve inputlara birer Name verecegiz.id="name" den sonra name="name" dedik inputlara. Sonra da controllerin icerisinde birer degiskende formdan gelen inputları asagıdaki gibi tutacagız. 
+        //SubmitTag() Add olması lazım ama aynı isim oldugu icin hata verir.
+        //Fakat get metodu kullandıgımız Add action ı ile post metodunda kullandıgımız Add action ının adları aynı olmamaktadır. Çünkü bir metot aynı sayıda parametreyi alarak yeniden aynı isimde kullanılamamaktadır.
+        //Dolayısıyla biz de metodumuzun adının farklı olmasını fakat davranış biçiminin aynı olmasını saglamak adına ActionName adında bir özellik kullanarak bu metodun da Add Action ı icinde davranıs sergilemesini sagladık. [ActionName("Add")] 
+        [HttpPost]
+        [ActionName("Add")]
+        public IActionResult SubmitTag()
+
+        {
+            var name = Request.Form["name"];
+            var displayName = Request.Form["displayName"];
+
+            //Submit işlemi gerçekleştikten sonra bizi Add.cshtml de tutmasını istiyoruz.
+            return View("Add");
         }
     }
 }
