d750bce236d0f91688abe13c379f607fa2d542e7
TahaYasinOzturk <tahayasinozturk@hotmail.com>
2023-04-24 23:13:15 +0300

    Update AdminTagsController

--- Bloggie/Bloggie.Web/Controllers/AdminTagsController.cs
+++ Bloggie/Bloggie.Web/Controllers/AdminTagsController.cs
@@ -1,10 +1,20 @@
-﻿using Bloggie.Web.Models.ViewModels;
+﻿using Bloggie.Web.Data;
+using Bloggie.Web.Models.Domain;
+using Bloggie.Web.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Bloggie.Web.Controllers
 {
     public class AdminTagsController : Controller
     {
+        private readonly BloggieDbContext bloggieDbContext;
+        //DbContext icin dependency injection işlemi gerçekleştirildi.
+
+        //db ye postla  veri göndercez önce ctor yapıyoz .using Bloggie.Web.Data; ekledi.BloggieDbContext bloggieDbContext isimde atadık aynı isimle sonra ctrl. yaptık ikinci olana tıkladık. creat field .. yazıyordu.
+        public AdminTagsController(BloggieDbContext bloggieDbContext)
+        {
+            this.bloggieDbContext = bloggieDbContext;
+        }
         //yapmıs oldugumuz MVC routing sekli sunun gibi olacaktır.  Admintags controller
         //Add de action tarafı [HttpGet] arka planda calısır.
         //http://localhost:154/AdminTags/Add
@@ -23,8 +33,8 @@ namespace Bloggie.Web.Controllers
         // inputlardan gelen verileri viewmodela yolladık (ara katmana )sonra controllera sonra DB ye gidecek. 
         //view üzerinden kullanıcının girecegi veri kadar olan kısım icin bir view model olusturduk. ve orjinal domain modeldeki kullandıgımız ihtiyacımız olan propertyleri icerisine ekledik. Viewda artık bu viewmodela erişmek icin en üst kısma bir model tanımlaması yapıp viewmodelimizin bulundugu namespace i tanımladık. sonra da inputlardaki degişiklikleri bu viewmodela aktarmak icin her input a birer asp-for property si ekledik.
         //bunun da amacı suydu, sayfanın en üstünde tanımlamış oldugumuz model icindeki propertylerden  hangilerini doldurmamız gerektigini tanımladıgımız alandı. yani kullanıcı name bölümüne  bişeyler yazdıgında viewmodel icinde bulunan name kısmı dolduruluyor olacaktır. Akabinde doldurulması gereken her yer doldurulduktan sonra submit işlemi gerçekleştiginde bu verileri view model üzerinden controller a gönderiyor olacagız.
-        //[ActionName("Add")] ve   Submittag i Add olarak degistirdik.
-        //breakpoint koyup inputa girdigimiz veriyi name ve displayname e atandıgını kontrol  ettik
+        //[ActionName("Add")] gerek kalmadı ve   Submittag i Add olarak degistirdik.
+        //breakpoint koyup inputa girdigimiz veriyi name ve displayname e atandıgını kontrol ettik
 
 
         [HttpPost]
@@ -36,8 +46,23 @@ namespace Bloggie.Web.Controllers
             //var name = Request.Form["name"];
             //var displayName = Request.Form["displayName"];
 
-            var name = addTagRequest.Name;
-            var displayName = addTagRequest.DisplayName;
+
+            //2.yöntemde yazdık yukarda ctor yaptık sonra burayı sildik
+            //var name = addTagRequest.Name;
+            //var displayName = addTagRequest.DisplayName;
+
+            //2.yöntemdensonra yazdık 
+            //tag e crl . yaptık using i ekledik using Bloggie.Web.Models.Domain;
+
+            var tag = new Tag
+            {
+                Name = addTagRequest.Name,
+                DisplayName = addTagRequest.DisplayName,
+            };
+
+            bloggieDbContext.Tags.Add(tag);
+            bloggieDbContext.SaveChanges();
+            //veri girişini yaptık CRUD daki Add işlemi gerçekleşti. siteden veri submitledik sonra  ssms de execute diyip gelenleri gördük.
 
             //Submit işlemi gerçekleştikten sonra bizi Add.cshtml de tutmasını istiyoruz.
             return View("Add");
